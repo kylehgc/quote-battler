59de73d4f15803884dd375021db331f8a77e4eb8
Kyle Christensen <kylehgc@gmail.com>
2022-12-07 21:01:30 -0500

    moved to new api

--- quote-chakra/src/Utils/api.js
+++ quote-chakra/src/Utils/api.js
@@ -1,35 +1,35 @@
-const queryUrl = 'https://quote-garden.herokuapp.com/api/v3/quotes/random'
+const queryUrl = 'https://api.quotable.io/random?maxLength=150'
 
-const getShortQuotes = (quotes, limit = 150) => {
-	const shortenedQuotes = quotes.filter(
-		(quote) => quote.quoteText.length < limit,
-	)
-	return shortenedQuotes
-}
-const getQuotes = async () => {
-	const params = `?count=10`
+// const getShortQuotes = (quotes, limit = 150) => {
+// 	const shortenedQuotes = quotes.filter(
+// 		(quote) => quote.quoteText.length < limit,
+// 	)
+// 	return shortenedQuotes
+// }
+const getQuote = async () => {
+	const params = `?maxLength=150'`
 	const response = await fetch(`${queryUrl}${params}`)
 	if (!response.ok) {
 		throw new Error(response.error)
 	}
-	const quotes = await response.json()
-	return quotes.data
+	const quote = await response.json()
+	return quote
 }
 const formatGameData = (quotes) => {
 	const gameData = quotes.slice(0, 2).map((quote) => {
 		return {
-			author: quote.quoteAuthor,
-			text: quote.quoteText,
-			genre: quote.quoteGenre,
+			author: quote.author,
+			text: quote.content,
+			genre: quote.tags[0],
 		}
 	})
 	return gameData
 }
 
 const getGameData = async (quotes = 2) => {
-	const fullQuotes = await getQuotes()
-	const shortenedQuotes = getShortQuotes(fullQuotes)
-	let formattedGameData = formatGameData(shortenedQuotes)
+	const quoteArray = await Promise.all([getQuote(), getQuote()])
+
+	let formattedGameData = formatGameData(quoteArray)
 	return formattedGameData
 }
 export default getGameData
